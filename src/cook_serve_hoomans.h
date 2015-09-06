3eebee12446a01674b453bde92a369eb70d77726
Marianne Panzenböck <marianne@marianne-panzenbocks-imac.local>
2015-09-06 04:46:58 +0200

    it's game.ios under Mac

--- src/cook_serve_hoomans.h
+++ src/cook_serve_hoomans.h
@@ -4,6 +4,8 @@
 
 #if defined(_WIN16) || defined(_WIN32) || defined(_WIN64)
 #	define CSH_GAME_ARCHIVE "data.win"
+#elif defined(__APPLE__)
+#	define CSH_GAME_ARCHIVE "game.ios"
 #else
 #	define CSH_GAME_ARCHIVE "game.unx"
 #endif
